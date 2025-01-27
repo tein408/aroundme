cd5d35ca918dbb476345b949d925ffa58ee793d3
Eunha Kang <75615404+tein408@users.noreply.github.com>
2025-01-27 20:45:00 +0900

    fix: Modify to indicate explicitly instead of asterisks

--- src/test/kotlin/com/aroundme/content/ContentControllerTest.kt
+++ src/test/kotlin/com/aroundme/content/ContentControllerTest.kt
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import java.time.LocalDateTime
 
