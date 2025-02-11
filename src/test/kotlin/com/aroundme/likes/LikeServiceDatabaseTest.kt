65d443edde43a5d5b4348ff9b0cebe394a263baf
Eunha Kang <75615404+tein408@users.noreply.github.com>
2025-02-11 22:45:38 +0900

    feat: Add a function to support decrease a like

--- src/test/kotlin/com/aroundme/likes/LikeServiceDatabaseTest.kt
+++ src/test/kotlin/com/aroundme/likes/LikeServiceDatabaseTest.kt
@@ -2,12 +2,14 @@ package com.aroundme.likes
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.test.context.ActiveProfiles
+import java.time.LocalDateTime
 
 @DataJpaTest
 @ActiveProfiles("test")
@@ -50,6 +52,31 @@ class LikeServiceDatabaseTest {
         val findLikes = likeRepository.findById(LikesId(contentId, userId)).orElse(null)
         assertEquals(contentId, findLikes?.likeId?.contentId)
         assertEquals(userId, findLikes?.likeId?.userId)
+    }
+
+    @Test
+    fun `should successfully decrease a like to database`() {
+        val contentId = 1L
+        val userId = 100L
+        val likeId = LikesId(contentId, userId)
+        val likes = Likes(likeId, LocalDateTime.now())
+        likeRepository.save(likes)
+
+        likeService.decreaseLikeCount(contentId, userId)
+
+        assertTrue(likeRepository.findById(likeId).isEmpty)
+    }
+
+    @Test
+    fun `should raise an exception when a pair of contentId and userId not contain like table`() {
+        val contentId = 1L
+        val userId = 100L
+
+        val exception = assertThrows<IllegalStateException> {
+            likeService.decreaseLikeCount(contentId, userId)
+        }
+
+        assertEquals("User $userId is not liked this content $contentId", exception.message)
     }
 
 }
--- src/test/kotlin/com/aroundme/likes/LikeServiceTest.kt
+++ src/test/kotlin/com/aroundme/likes/LikeServiceTest.kt
@@ -1,6 +1,8 @@
 package com.aroundme.likes
 
+import io.mockk.Runs
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -49,6 +51,37 @@ class LikeServiceTest {
         assertEquals("User $userId already liked this content $contentId", exception.message)
         verify { likeRepository.findLikesByLikeId(likeId) }
         verify(exactly = 0) { likeRepository.save(any()) }
+    }
+
+    @Test
+    fun `should successfully delete function call when user request like deletion`() {
+        val contentId = 1L
+        val userId = 100L
+        val likeId = LikesId(contentId, userId)
+        val likes = Likes(likeId, LocalDateTime.now())
+        every { likeRepository.findLikesByLikeId(likeId) } returns likes
+        every { likeRepository.delete(likes) } just Runs
+
+        likeService.decreaseLikeCount(contentId, userId)
+
+        verify { likeRepository.findLikesByLikeId(likeId) }
+        verify { likeRepository.delete(likes) }
+    }
+
+    @Test
+    fun `should throw an exception when user has not liked the content`() {
+        val contentId = 1L
+        val userId = 100L
+        val likeId = LikesId(contentId, userId)
+        every { likeRepository.findLikesByLikeId(likeId) } returns null
+
+        val exception = assertThrows<IllegalStateException> {
+            likeService.decreaseLikeCount(contentId, userId)
+        }
+
+        assertEquals("User $userId is not liked this content $contentId", exception.message)
+        verify { likeRepository.findLikesByLikeId(likeId) }
+        verify(exactly = 0) { likeRepository.delete(any()) }
     }
 
 }
